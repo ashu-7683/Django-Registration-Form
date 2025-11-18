229da82f36b2a81ae618849f6c9bf279ccde7064
unknown <ashutoshkhilar5@gmail.com>
2025-11-18 21:16:54 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -8,5 +8,22 @@ def registration(request):
     EUMFO=UserMF()
     EPMFO=ProfileMF()
     d={'EUMFO':EUMFO,'EPMFO':EPMFO}
+    if request.method=='POST':
+        UMFDO=UserMF(request.POST)
+        PMFDO=ProfileMF(request.POST,request.FILES)
+        if UMFDO.is_valid() and PMFDO.is_valid():
+            NSU=UMFDO.save()
+            password=UMFDO.cleaned_data['password']
+            NSU.set_password(password)
+            NSU.save()
+            NPS=PMFDO.save()
+            NPS.username=NSU
+            NPS.save()
+            #show the registered data
+            return HttpResponse(str(EUMFO.cleaned_data))
+            
+        else:
+            return HttpResponse('Invalid Data')
+    
     
     return render(request,'registration.html',d)
