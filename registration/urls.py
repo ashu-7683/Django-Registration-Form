8c3b13d3200a02bf2071b62fd647a6d53474db94
unknown <ashutoshkhilar5@gmail.com>
2025-11-24 20:55:05 +0530

    second commit

--- registration/urls.py
+++ registration/urls.py
@@ -22,6 +22,12 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/',registration,name='registration')
+    path('registration/',registration,name='registration'),
+    path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
+    path('display_details/',display_details,name='display_details'),
+    path('change_password/',change_password,name='change_password'),
+    path('reset_password/',reset_password,name='reset_password'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
